f0051f9e91f1ec526de5c9235f454972ad3aa0c6
Vishnu Benny <vbenny@falabella.cl>
2024-07-07 23:52:12 +0530

    Algorithm for selection sort with linear search

---
+++ src/SelectionSort.kt
@@ -0,0 +1,33 @@
+fun findMaxIndex(arr: IntArray, start: Int, end: Int): Int {
+    var maxIndex = start
+    for (i in start + 1 until end) {
+        if (arr[i] > arr[maxIndex]) {
+            maxIndex = i
+        }
+    }
+    return maxIndex
+}
+
+fun swap(arr: IntArray, i: Int, j: Int) {
+    val temp = arr[i]
+    arr[i] = arr[j]
+    arr[j] = temp
+}
+
+fun selectionSort(arr: IntArray) {
+    val n = arr.size
+    for (i in n - 1 downTo 1) {
+        val maxIndex = findMaxIndex(arr, 0, i + 1)
+        if (maxIndex != i) {
+            swap(arr, maxIndex, i)
+        }
+    }
+}
+
+fun main() {
+    val numbers = intArrayOf(64, 25, 12, 22, 11)
+    println("Unsorted array: ${numbers.joinToString(", ")}")
+
+    selectionSort(numbers)
+    println("Sorted array: ${numbers.joinToString(", ")}")
+}
