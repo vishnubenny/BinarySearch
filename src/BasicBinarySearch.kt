b75f4e6bca9d4a47f87c881200448ee19ace0152
Vishnu Benny <vbenny@falabella.cl>
2024-07-07 23:11:27 +0530

    Initial commit

---
+++ src/BasicBinarySearch.kt
@@ -0,0 +1,28 @@
+fun binarySearch(arr: IntArray, target: Int): Int {
+    var start = 0
+    var end = arr.size - 1
+
+    while (start <= end) {
+        val mid = start + (end - start) / 2
+
+        when {
+            arr[mid] == target -> return mid
+            arr[mid] < target -> start = mid + 1
+            else -> end = mid - 1
+        }
+    }
+
+    return -1 // Element not found
+}
+
+fun main() {
+    val numbers = intArrayOf(1, 3, 5, 7, 9, 11)
+    val target = 7
+    val index = binarySearch(numbers, target)
+
+    if (index != -1) {
+        println("Element found at index: $index")
+    } else {
+        println("Element not found")
+    }
+}
